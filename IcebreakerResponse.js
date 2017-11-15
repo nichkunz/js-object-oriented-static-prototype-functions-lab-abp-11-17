3e39fbe7976ba0b13fa2b338f7ad07f5035e51a6
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-15 14:04:44 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -3,5 +3,7 @@ function IcebreakerResponse(email){
 }
 
 IcebreakerResponse.BatchCreate = function(emails){
-  return emails.map(function )
+  return emails.map(function (email){
+    return new IcebreakerResponse(email[0]);
+  })
 }
